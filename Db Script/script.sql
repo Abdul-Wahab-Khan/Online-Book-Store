41bd3b44dd1aa18c866da6489111555b8e20b411
root <root@localhost.localdomain>
2020-06-03 12:02:23 +0430

    Database script done

---
+++ Db Script/script.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE IF NOT EXISTS `online_book_store`;
+USE `online_book_store`;
+
+CREATE TABLE IF NOT EXISTS `register_user` (
+  `register_user_id` int(11) PRIMARY KEY,
+  `user_name` varchar(250) NOT NULL,
+  `user_email` varchar(250) NOT NULL,
+  `user_password` varchar(250) NOT NULL,
+  `user_activation_code` varchar(250) NOT NULL,
+  `user_email_status` enum('not verified','verified') NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2;
+
+CREATE TABLE IF NOT EXISTS `Book` (
+    `book_id` INT PRIMARY KEY,
+    `name` varchar(250) NOT NULL,
+    `description` varchar(500),
+    `price` int default 0,  
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS `Borrowed_Book` (
+    `book_id` int,
+    `register_user_id` int,
+    `borrowed_date` date NOT NULL,
+    `date_to_return` date,
+    FOREIGN KEY (book_id) REFERENCES Book(book_id),
+    FOREIGN KEY (register_user_id) REFERENCES register_user(register_user_id)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS `Uploaded_Book` (
+    `book_id` int,
+    `register_user_id` int,
+    `upload_date` date NOT NULL,
+    FOREIGN KEY (book_id) REFERENCES Book(book_id),
+    FOREIGN KEY (register_user_id) REFERENCES register_user(register_user_id)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS `Downloaded_Book` (
+    `book_id` int,
+    `register_user_id` int,
+    `download_date` date NOT NULL,
+    `paid` int default 0,
+    FOREIGN KEY (book_id) REFERENCES Book(book_id),
+    FOREIGN KEY (register_user_id) REFERENCES register_user(register_user_id)
+) ENGINE=InnoDB;
+
